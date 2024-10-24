2d77d66af4b57c23d7735504895cba881f916737
Avril Albistur <aalbistur27@gmail.com>
2024-10-24 08:45:57 -0300

    pagina principal con los personajes. LISTA

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const apikey = "a41f305d0402b8faa73dab310ee06012";
+const ts = "2024";
+const hash = "c86b43c393cf34372423f5f4bac297f3";
+const mainURL = `https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}`; //URL PARA TRAER TODOS LOS PERSONAJES
+const characterInfo = `https://gateway.marvel.com:443/v1/public/characters/${characterId}?apikey=a41f305d0402b8faa73dab310ee06012&ts=2024&hash=c86b43c393cf34372423f5f4bac297f3&characterId=1017100`
+
+//FUNCION PARA HACER EL FETCH A LA API DE MARVEL
+let getMarvelData = (URL) => {
+  let data = [];
+  return fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      } else {
+        return response.json();
+      }
+    })
+    .then((response) => {
+      data = response.data.results;
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+// FUNCION PARA CREAR LAS CARDS DE LOS PERSONAJES
+let createCharacterCards = (characters) => {
+  let HTMLtoappend = ``;
+
+  characters.forEach((character) => {
+    let {id,name, description, thumbnail: {extension,path}} = character
+    HTMLtoappend += `
+      <div class="card col-4 m-3 bg-dark-subtle" style="width: 18rem;">
+        <img src="${path+`.`+extension}" class="card-img-top overflow-hidden" style="height: 10rem;" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text overflow-auto" style="height: 5rem;">${description || `this character has no description`}</p>
+          <a href="#" class="btn btn-outline-danger " id="${id}">Mas info.</a>
+        </div>
+      </div>
+      `;
+  });
+  document.getElementById('container').innerHTML = HTMLtoappend;
+};
+
+// getMarvelData(mainURL)
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  getMarvelData(mainURL)
+  .then(characters =>{
+    createCharacterCards(characters);
+  })
+})
