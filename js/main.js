8087461dd9f9f91584f810dea4a18a799c63269e
Avril Albistur <aalbistur27@gmail.com>
2024-10-24 21:00:58 -0300

    commit final

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const apikey = "a41f305d0402b8faa73dab310ee06012";
 const ts = "2024";
 const hash = "c86b43c393cf34372423f5f4bac297f3";
-const mainURL = `https://gateway.marvel.com:443/v1/public/characters?apikey=${apikey}&ts=${ts}&hash=${hash}`; //URL PARA TRAER TODOS LOS PERSONAJES
+const mainURL = `https://gateway.marvel.com:443/v1/public/characters?apikey=a41f305d0402b8faa73dab310ee06012&ts=2024&hash=c86b43c393cf34372423f5f4bac297f3`; //URL PARA TRAER TODOS LOS PERSONAJES
 
 //FUNCION PARA HACER EL FETCH A LA API DE MARVEL
 let getMarvelData = (URL) => {
